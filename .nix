97df7b12776a7f2108f67b7113622789253e259d
Brooklyn Zelenka <hello@brooklynzelenka.com>
2020-07-18 01:48:39 +0000

    Add .nix

---
+++ .nix
@@ -0,0 +1,39 @@
+let
+  inherit (import <nixpkgs> {}) fetchFromGitHub;
+
+  stablepkgs = fetchFromGitHub {
+    owner  = "NixOS";
+    repo   = "nixpkgs";
+
+    rev    = "aaa66d8d887c73f643246ac1a684fcb1521543b8";
+    sha256 = "0qvrhc7hv8h4yqa4jh64y6v5j3nza53ivkbq6j72g434c3yp2h50";
+  };
+
+  unstablepkgs = fetchFromGitHub {
+    owner  = "NixOS";
+    repo   = "nixpkgs";
+
+    rev    = "6148f6360310366708dff42055a0ba0afa963101";
+    sha256 = "1j91hxfak1kark776klszakvg0a9yv77p7lnrvj7g32v6g20qdsk";
+  };
+
+  stable   = import stablepkgs   {};
+  unstable = import unstablepkgs {};
+in
+  stablepkgs.stdenv.mkDerivation {
+    name = "fission-drive";
+
+    buildInputs = [
+      stable.elmPackages.elm
+
+      stable.lolcat
+      stable.figlet
+
+      unstable.just
+    ];
+
+    shellHook = ''
+      echo "Welcome to the"
+      figlet "Fission Drive Shell" | lolcat -a -s 50
+    '';
+}
